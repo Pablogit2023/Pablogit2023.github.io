d72e44fdafe3452e76f70b4fab58613ef56ae80a
Pablogit2023 <pablolass@gmail.com>
2023-06-03 20:16:11 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const boton1 = document.getElementById("boton1");
-const boton2 = document.getElementById("boton2");
-const dato1 = document.getElementById('ingre1');
-const dato2 = document.getElementById('ingre2');
-let texto1;
 
-dato1.addEventListener("change", function() {
-        dato2.value = dato1.value;
-        /*boton2.click();*/
-    })
+
+//const boton1 = document.getElementById("boton1");
+//const boton2 = document.getElementById("boton2");
+//const dato1 = document.getElementById('ingre1');
+//const dato2 = document.getElementById('ingre2');
+//const boton3 = document.getElementById('boton3');
+//let texto1;
+
+//boton2.addEventListener("click", function () {
+//    /*dato2.value = "nover"*/
+//    dato1.addEventListener("change", function () {
+//        dato2.value = dato1.value;
+//        /*boton2.click();*/
+//    });
+//});
+
+
+
+//boton3.addEventListener("click", function () {
+//    const $messageForm = document.getElementById("formElem");
+//    const $messageBox = document.getElementById("nombre");
+//    console.log($messageBox.value);
+//    $messageForm.submit(event => {
+//        event.preventDefault();
+//        document.getElementById("ingre2").value = $messageBox.value;
+//    });
+//})
+
+//$(function () {
+
+//    // obtaining DOM elements from the Chat Interface
+//    const $messageForm = $("#formElem");
+//    const $messageBox = $("#nombre");
+   
+//});
