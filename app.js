de325c61692a2a4e1318d17c95aea3e519f522e5
Pablogit2023 <pablolass@gmail.com>
2023-06-27 19:27:46 -0300

    Initial commit

--- app.js
+++ app.js
@@ -7,25 +7,66 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+/*const mostrar = document.getElementById('cantidad');*/
+
+var conectados = 0;
+var maximo = 12;
+
 io.on('connection', (socket) => {
-    console.log('Un usuario se ha conectado');
+    conectados += 1;
+    console.log(`Un usuario se ha conectado: ${socket.id}, total: ${conectados}`);
+
+    if (conectados < maximo + 1) {
+        if (conectados < 2) {
+            socket.emit('chat', 'bienvenido, primer jugador')
+        } else
+            socket.emit('chat', 'bienvenido, hay ' + conectados + ' jugadores conectados');
+        io.emit('chat', 'AGRE' + conectados + 'GADOS');
+    } else
+        socket.emit('chat', 'completo');
+
     socket.on('disconnect', () => {
-        console.log('Ususario desconectado')
+        conectados -= 1;
+        if (conectados < 1) {
+            maximo = 12
+        };
+        console.log(`Ususario desconectado: ${socket.id}, total: ${conectados}`);
+        io.emit('chat', 'INCO' + conectados + 'NECTADO');
     });
+    //socket.on('create', function (room) {
+    //    socket.join(room);
+    //});
+    //socket.on('join', (room) => {
+    //    console.log(`Socket ${socket.id} joining ${room}`);
+    //    socket.join(room);
+    //});
+    /*socket.join('some room')*/
     //socket.on('chat', (msg) => {
     //    console.log('Mensaje: ' + msg)
     //});
     socket.on('chat', (msg) => {
-        io.emit('chat', msg)
+        if (msg.includes('MAXIMO')) {
+            quitar = msg.split(' ');
+            maximo = quitar[1];
+        };
+        if (conectados < maximo + 1) {
+            socket.broadcast.emit('chat', msg)
+        } else
+            socket.emit('chat', 'completo');
     });
 });
 
 app.get('/', (req, res) => {
     //res.send('<hi>Aplicacion de Chat</hi>')
-    res.sendFile(`${__dirname}/index.html`)
+    if (conectados < maximo) {
+        res.sendFile(`${__dirname}/index.html`)
+    } else
+        /*res.send('<hi id="completo">SALON COMPLETO</hi>') */
+        res.sendFile(`${__dirname}/indexlleno.html`)
+
 });
 
 server.listen(3000, () => {
     console.log('Servidor corriendo en http://localhost:3000')
-    
+
 });
